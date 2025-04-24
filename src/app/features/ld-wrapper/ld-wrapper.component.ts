8e41824294d55384b0c3ad3088bb194458aa9d10
Carlos Henrique <carlosmvsep3@hotmail.com>
2025-04-24 18:13:20 -0300

    #Ld-wrapper componentizado

---
+++ src/app/features/ld-wrapper/ld-wrapper.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LdHeaderComponent } from '../ld-header/ld-header.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-ld-wrapper',
+  imports: [LdHeaderComponent],
+  templateUrl: './ld-wrapper.component.html',
+  styleUrl: './ld-wrapper.component.scss'
+})
+export class LdWrapperComponent {
+
+}
